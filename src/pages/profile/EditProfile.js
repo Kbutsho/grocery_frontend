a6401243c4835799ba52d7487ff6be044faeaa5e
Kaushik Biswas <kbutsho@gmail.com>
2022-12-06 00:21:50 +0600

    update

--- src/pages/profile/EditProfile.js
+++ src/pages/profile/EditProfile.js
@@ -54,12 +54,12 @@ const EditProfile = () => {
       ) {
         const image = new FormData();
         image.append("file", profileImage);
-        image.append("cloud_name", "zinotrust");
-        image.append("upload_preset", "wk66xdkq");
+        image.append("cloud_name", "dtfatogno");
+        image.append("upload_preset", "ondodjlo");
 
         // First save image to cloudinary
         const response = await fetch(
-          "https://api.cloudinary.com/v1_1/zinotrust/image/upload",
+          "https://api.cloudinary.com/v1_1/dtfatogno/image/upload",
           { method: "post", body: image }
         );
         const imgData = await response.json();
